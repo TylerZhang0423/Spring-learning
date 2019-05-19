e3387f229b17394f4800bb7e13569e8de8f30ee1
TylerZhang0423 <516492597@qq.com>
2019-05-19 22:22:01 +0800

    201905192221

--- Spring01/src/com/spring/Helloworld/HelloWorld.java
+++ Spring01/src/com/spring/Helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.spring.Helloworld;
 
 public class HelloWorld {
-
+	public void show() {
+		System.out.println("Hello World!!!");
+	}
 }
--- Spring01/src/com/spring/test/Main.java
+++ Spring01/src/com/spring/test/Main.java
@@ -1,10 +1,16 @@
 package com.spring.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.Helloworld.HelloWorld;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+		HelloWorld testHelloWorld=(HelloWorld) ac.getBean("testHelloWorld");
+		testHelloWorld.show();
 	}
-
 }
