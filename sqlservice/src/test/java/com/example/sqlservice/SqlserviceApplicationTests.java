605956428e483ba7213877bc4fa21debb230d148
TylerZhang0423 <516492597@qq.com>
2019-07-25 15:22:55 +0800

    201907251522

---
+++ sqlservice/src/test/java/com/example/sqlservice/SqlserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
