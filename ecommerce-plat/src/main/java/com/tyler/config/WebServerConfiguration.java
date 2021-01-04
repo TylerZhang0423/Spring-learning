046b6ca981a2475550838cc3bdcde13d493c3958
TylerZhang0423 <516492597@qq.com>
2021-01-04 23:24:43 +0800

    202101042324

--- ecommerce-plat/src/main/java/com/tyler/config/WebServerConfiguration.java
+++ ecommerce-plat/src/main/java/com/tyler/config/WebServerConfiguration.java
@@ -1,5 +1,9 @@
 package com.tyler.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
@@ -13,7 +17,17 @@ import org.springframework.stereotype.Component;
 public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
-    public void customize(ConfigurableWebServerFactory factory) {
-
+    public void customize(ConfigurableWebServerFactory configurableWebServerFactory) {
+        //使用工厂类提供的接口定制化tomcat connector
+        ((TomcatServletWebServerFactory)configurableWebServerFactory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+                //定制化keepAliveTimeout,设置30秒内没有请求则服务端自动断开keepalive链接
+                protocol.setKeepAliveTimeout(30000);
+                //当客户端发送超过10000个请求则自动断开keepalive链接
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
     }
 }
